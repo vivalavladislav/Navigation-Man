ab2c32966dabd06736eb508a049858343cd9e386
A$AP GIT <vlad.sviatetskyi@gmail.com>
2017-05-08 18:30:57 +0300

    auto scale for dima

---
+++ Navigation Man/Assets/AutoScale.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class AutoScale : MonoBehaviour 
+{
+	public GameObject[] constellations;
+	public Vector3 scale;
+
+	public void Do()
+	{
+		foreach (GameObject con in constellations)
+		{
+			// child iteration, magic
+			foreach(Transform star in con.transform) 
+			{
+				//MeshRenderer mr = star.gameObject.GetComponent<MeshRenderer>();
+				//mr.material.color = Color.green;
+				star.localScale = scale;
+			}
+		}
+	}
+}
+
+
+
+[CustomEditor(typeof(AutoScale))]
+public class AutoScaleEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		var t = target as AutoScale;
+		if (GUILayout.Button("change scale"))
+		{
+			t.Do();
+		}
+	}
+}
+
+
